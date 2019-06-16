bca59552775df1ce913cd6667f68df649c90bddf
shreyanshn <shreyanshnawlakha@gmail.com>
2019-06-15 20:39:05 -0400

    Components complete

---
+++ src/components/About.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+        <section className="ftco-about ftco-counter img ftco-section" id="about-section">
+        <div className="container">
+            <div className="row d-flex">
+            <div className="col-md-6 col-lg-5 d-flex">
+                <div className="img-about img d-flex align-items-stretch">
+                <div className="overlay" />
+                <div className="img d-flex align-self-stretch align-items-center" style={{backgroundImage: 'url(images/about-1.jpg)'}}>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-6 col-lg-7 pl-lg-5 py-5">
+                <div className="row justify-content-start pb-3">
+                <div className="col-md-12 heading-section ftco-animate">
+                    <span className="subheading">Welcome</span>
+                    <h2 className="mb-4" style={{fontSize: '34px', textTransform: 'capitalize'}}>About Me</h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary
+                    regelialia. It is a paradisematic country, in which roasted parts of sentences fly 
+                    into your mouth.</p>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-md-6">
+                    <div className="media block-6 services d-block ftco-animate">
+                    <div className="icon"><span className="flaticon-analysis" /></div>
+                    <div className="media-body">
+                        <h3 className="heading mb-3">Full Stack Development</h3>
+                        <p>A small river named Duden flows by their place and supplies.</p>
+                    </div>
+                    </div> 
+                </div>
+                <div className="col-md-6">
+                    <div className="media block-6 services d-block ftco-animate">
+                    <div className="icon"><span className="flaticon-analysis" /></div>
+                    <div className="media-body">
+                        <h3 className="heading mb-3">Machine Learning</h3>
+                        <p>A small river named Duden flows by their place and supplies.</p>
+                    </div>
+                    </div> 
+                </div>
+                <div className="col-md-6">
+                    <div className="media block-6 services d-block ftco-animate">
+                    <div className="icon"><span className="flaticon-analysis" /></div>
+                    <div className="media-body">
+                        <h3 className="heading mb-3">Data Analysis</h3>
+                        <p>A small river named Duden flows by their place and supplies.</p>
+                    </div>
+                    </div> 
+                </div>
+                <div className="col-md-6">
+                    <div className="media block-6 services d-block ftco-animate">
+                    <div className="icon"><span className="flaticon-analysis" /></div>
+                    <div className="media-body">
+                        <h3 className="heading mb-3">Predictive Analysis</h3>
+                        <p>A small river named Duden flows by their place and supplies.</p>
+                    </div>
+                    </div> 
+                </div>
+                </div>
+                <div className="counter-wrap ftco-animate d-flex mt-md-3">
+                <div className="text p-4 pr-5 bg-primary">
+                    <p className="mb-0">
+                    <span className="number" data-number={8}>0</span>
+                    <span>Finished Projects</span>
+                    </p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+    )
+  }
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+        <footer className="ftco-footer ftco-section">
+        <div className="container">
+            <div className="row mb-5">
+            <div className="col-md">
+                <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">Lets talk about</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                    <li className="ftco-animate"><a href="#"><span className="icon-twitter" /></a></li>
+                    <li className="ftco-animate"><a href="#"><span className="icon-facebook" /></a></li>
+                    <li className="ftco-animate"><a href="#"><span className="icon-instagram" /></a></li>
+                </ul>
+                </div>
+            </div>
+            <div className="col-md">
+                <div className="ftco-footer-widget mb-4 ml-md-4">
+                <h2 className="ftco-heading-2">Links</h2>
+                <ul className="list-unstyled">
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Home</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />About</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Services</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Projects</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Contact</a></li>
+                </ul>
+                </div>
+            </div>
+            <div className="col-md">
+                <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">Services</h2>
+                <ul className="list-unstyled">
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Web Design</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Web Development</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Business Strategy</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Data Analysis</a></li>
+                    <li><a href="#"><span className="icon-long-arrow-right mr-2" />Graphic Design</a></li>
+                </ul>
+                </div>
+            </div>
+            <div className="col-md">
+                <div className="ftco-footer-widget mb-4">
+                <h2 className="ftco-heading-2">Have a Questions?</h2>
+                <div className="block-23 mb-3">
+                    <ul>
+                    <li><span className="icon icon-map-marker" /><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                    <li><a href="#"><span className="icon icon-phone" /><span className="text">+2 392 3929 210</span></a></li>
+                    <li><a href="#"><span className="icon icon-envelope" /><span className="text">info@yourdomain.com</span></a></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-md-12 text-center">
+                <p>{/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
+                Copyright © All rights reserved | This template is made with <i className="icon-heart color-danger" aria-hidden="true" /> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}</p>
+            </div>
+            </div>
+        </div>
+        </footer>
+
+    )
+  }
+}
